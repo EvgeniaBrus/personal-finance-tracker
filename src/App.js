9fd20f47c044fb4c26b65f155befeb9e03e0d0ab
EvgeniaBrus <evgeniabrusnitskaya@gmail.com>
2024-08-28 16:57:10 +0100

    test space

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Let her cook!</p>
+        <p>Let her cook! </p>
       </header>
     </div>
   );
